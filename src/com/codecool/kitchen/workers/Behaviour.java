94d5b3fa843723a0e4bc9b61c92ec0513809a3a0
Stefan Gherman <scgheman@gmail.com>
2020-04-27 02:04:59 +0300

    Add Employee class

---
+++ src/com/codecool/kitchen/workers/Behaviour.java
@@ -0,0 +1,4 @@
+package com.codecool.kitchen.workers;
+
+public interface Behaviour {
+}
--- src/com/codecool/kitchen/workers/Employee.java
+++ src/com/codecool/kitchen/workers/Employee.java
@@ -1,6 +1,49 @@
 package com.codecool.kitchen.workers;
 
+import java.util.Date;
+
 public class Employee {
 
+    //Fields
+    private String name;
+    private Date birthYear;
+    private float salary;
 
+    //Constructor
+    public Employee(String name, Date birthYear, float salary) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Date birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", birthYear=" + birthYear +
+                ", salary=" + salary;
+    }
 }
